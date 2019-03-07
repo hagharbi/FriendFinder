93a820fe3db79a9c75b417869ac2bdde33c983ad
Santino <ha.agharbi@gmail.com>
2019-03-06 22:02:28 -0800

    added initial friend's application data

---
+++ app/data/friends.js
@@ -0,0 +1,23 @@
+// APPLICATION DATA
+// Saves application's data as an array of objects
+var friends = [
+    {
+        "name": "Ahmed",
+        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores": [
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
+    }
+];
+
+// Export the array to other files 
+module.exports = friends;
