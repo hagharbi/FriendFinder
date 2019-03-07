12a3d20397b751b9bc7341e1acd453c42d011010
Santino <ha.agharbi@gmail.com>
2019-03-06 22:01:01 -0800

    created server and setup for express app

---
+++ server.js
@@ -0,0 +1,22 @@
+// DEPENDENCIES
+var express = require("express");
+// var bodyParser = require("body-parser");
+
+// EXPRESS CONFIGURATION
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+// EXPRESS APP SETUP
+// Sets up the Express App to handle data parsing
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+
+// ROUTER
+// These routes direct our server to a series of route files
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+
+// LISTENER
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+});
